2da277ea4553b88aae1a0256eb1c92085d418bc3
Sergey Bulyno <s_bulyno@wargaming.net>
2016-08-23 23:11:01 +0300

    Create project.

---
+++ TestFavorites/TestFavorites/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestFavorites
+//
+//  Created by Sergey Bulyno on 8/23/16.
+//  Copyright © 2016 Sergey Bulyno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestFavorites/TestFavorites/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestFavorites
+//
+//  Created by Sergey on 8/23/16.
+//  Copyright © 2016 Sergey Bulyno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
