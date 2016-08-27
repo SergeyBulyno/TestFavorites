f3e363437a0a5ceb933a4312fe7637c812789843
Sergey Bulyno <s_bulyno@wargaming.net>
2016-08-27 14:44:24 +0300

    Add assets.

---
+++ TestFavorites/TestFavorites/Classes/Common/SBBaseViewController.h
@@ -0,0 +1,15 @@
+//
+//  SBBaseViewController.h
+//  TestFavorites
+//
+//  Created by Sergey Bulyno on 8/23/16.
+//  Copyright © 2016 Sergey Bulyno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBBaseViewController : UIViewController
+
+- (instancetype)initWithViewModel:(id)viewModel;
+
+@end
---
+++ TestFavorites/TestFavorites/Classes/QuestionsScreen/QuestionsTab/Cells/SBQuestionCellItem.m
@@ -0,0 +1,15 @@
+//
+//  SBQuestionCellItem.m
+//  TestFavorites
+//
+//  Created by Sergey Bulyno on 8/24/16.
+//  Copyright © 2016 Sergey Bulyno. All rights reserved.
+//
+
+#import "SBQuestionCellItem.h"
+
+@implementation SBQuestionCellItem
+
+
+
+@end
