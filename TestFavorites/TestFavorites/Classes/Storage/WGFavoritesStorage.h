ff922d05e86f4e15e4cbe440e8e90a06af88c8c4
Sergey Bulyno <s_bulyno@wargaming.net>
2016-08-29 10:41:44 +0300

    Add persistent storage for favourites.

---
+++ TestFavorites/TestFavorites/Classes/Storage/WGFavoritesStorage.h
@@ -0,0 +1,13 @@
+//
+//  WGFavoritesStorage.h
+//  TestFavorites
+//
+//  Created by Sergey on 8/29/16.
+//  Copyright © 2016 Sergey Bulyno. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WGFavoritesStorage : NSObject
+
+@end
---
+++ TestFavorites/TestFavorites/Classes/Storage/WGFavoritesStorage.m
@@ -0,0 +1,13 @@
+//
+//  WGFavoritesStorage.m
+//  TestFavorites
+//
+//  Created by Sergey on 8/29/16.
+//  Copyright © 2016 Sergey Bulyno. All rights reserved.
+//
+
+#import "WGFavoritesStorage.h"
+
+@implementation WGFavoritesStorage
+
+@end
